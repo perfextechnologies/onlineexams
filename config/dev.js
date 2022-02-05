5a92e1d090320a96d95a19ffe79da722ddefd254
Praneeth <perfextechnologies@gmail.com>
2022-02-05 15:52:46 +0530

    first commit for UI dev.js

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    MONOGOURI: "mongodb+srv://deepak:dipak@1243@cluster0.o7rmv.mongodb.net/HobbyRooms?retryWrites=true&w=majority",
+    jwtPrivateKey: "santoshkumar",
+    SENDGRID_API_KEY: "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",
+    EMAIL: "silarapusantosh@gmail.com",
+  };
